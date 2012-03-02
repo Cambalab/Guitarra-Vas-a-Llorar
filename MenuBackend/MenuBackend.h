a735cf3ea29bd9dd302bd48e4188ebee591f5fc2
Adrian Pardini <publico@tangopardo.com.ar>
2012-03-02 13:25:03 -0300

    prevenir bucle infinito en MenuBackend.h
    Al llamar a use() o moveDown() si hay un ciclo chequear si dimos una vuelta completa.
    Por ejemplo:
        m2<-m0->m1->m2

--- MenuBackend/MenuBackend.h
+++ MenuBackend/MenuBackend.h
@@ -66,7 +66,7 @@ public:
 		if (after->remember_parent) { 
             after->setBefore(this);
             MenuItem *node = after->getRight();
-            while(node && (node != after)){
+            while(node && (node != after) && (node != this)){
                 node->setBefore(this);
                 node = node->getRight();
             }
